6bdfed9f2ce8cc961f7f7f04d06c7aca4362dea3
Kevin Chen <k39chen@gmail.com>
2014-08-21 12:34:31 -0400

    Meteorized the cat game! Doesn't work yet tho...

---
+++ client/js/index.js
@@ -0,0 +1,16 @@
+// Meteor subscriptions
+// ...
+
+// define router configuration
+// ...
+
+// define routes
+// ...
+
+Router.map(function(){
+	this.route("game", {
+		path: "/",
+		template: "game"
+		/* yieldTemplates */
+	});
+});
